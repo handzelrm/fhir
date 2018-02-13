f92144317205bffd2d979e115fa65b15df616bec
handzelrm <robert.m.handzel@gmail.com>
2018-02-13 16:59:01 -0500

    added argsparse options at command line

--- fhirgenerator/fpar_generator.py
+++ fhirgenerator/fpar_generator.py
@@ -5,8 +5,10 @@ from generatetask import GenerateTask
 import pandas as pd
 import os
 import time
+import argparse
 
-def main():
+
+def generate_fpar_resources(timeit):
     t0 = time.time()
     obs_dict = GenerateObservationDict()
     obs_vitals = GenerateObservation(obs_dict.observation_dict,Patient=obs_dict.Patient)
@@ -18,27 +20,37 @@ def main():
     task = GenerateTask(Patient=obs_vitals.Patient,Practitioner_agent=obs_vitals.Practitioner, Encounter=obs_vitals.Encounter)
     task.complete_task()
     
-
-    # if os.path.exists('./patient_list.xlsx'):
-    #     df = pd.read_excel('./patient_list')
-    # else:
-    #     df = pd.DataFrame(columns=['name','id'])
-    
-    # with open('./patient_list','a') as f:
-    #     f.write(f'{task.Patient.id}')
     name = f'{task.Patient.name[0].family},{task.Patient.name[0].given[0]}'
     pt_id = f'{task.Patient.id}'
 
     t1 = time.time()
-    print(f'{t1-t0:.2f} seconds')
+
+    if time:
+        print(f'{t1-t0:.2f} seconds')
 
     return name, pt_id
 
-if __name__ == '__main__':
+def loop_generator(num,output,timeit):
     name_list = []
     id_list = []
-    for i in range(100):
-        name, pt_id = main()
+    for i in range(num):
+        name, pt_id = generate_fpar_resources(timeit)
         name_list.append(name)
         id_list.append(pt_id)
-    pd.DataFrame({'name':name_list,'id':id_list}).to_excel('../fpar/patient_list.xlsx',index=False)
+    if output:
+        pd.DataFrame({'name':name_list,'id':id_list}).to_excel('../fpar/patient_list.xlsx',index=False)
+    
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('num', help="Number of fpar patients to create.", type=int)
+    parser.add_argument('-o','--output', help='Output the result to an excel file', action='store_true')
+    parser.add_argument('-t', '--timeit', help='Print the time it takes to run each patient.', action='store_true')
+    args = parser.parse_args()
+
+    loop_generator(args.num, args.output, args.timeit)
+
+if __name__ == '__main__':
+    main()
+    
